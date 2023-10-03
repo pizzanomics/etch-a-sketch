54e8738e7145bc78d0d680f6fdeec388ead48270
pizzanomics <141937353+pizzanomics@users.noreply.github.com>
2023-10-03 21:50:38 +0100

    Create JS for building canvas area

---
+++ myscript.js
@@ -0,0 +1,20 @@
+const canvasInitialiser = document.querySelector('#canvas-initialiser')
+const canvas = document.querySelector('#canvas')
+
+
+canvasInitialiser.addEventListener('click', () => {
+    
+        canvas.innerHTML = '';
+        let totalSquares = 16 * 16
+
+        for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.textContent = "foobar"
+        square.classList.add('grid-square');
+        canvas.appendChild(square);
+        }
+
+        canvas.style.border = "2px solid black"
+    
+});
+
